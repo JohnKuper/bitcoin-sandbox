1cd201ca9cb564aa385e3aa14e46e9dba36dcb2c
Dmitry Korobeinikov <korobeinikov@yamoney.ru>
2018-05-06 15:30:15 +0300

    Added stubs for fragments.

--- app/src/main/java/com/kaizendeveloper/bitcoinsandbox/ui/MainActivity.kt
+++ app/src/main/java/com/kaizendeveloper/bitcoinsandbox/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.kaizendeveloper.bitcoinsandbox.ui
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +13,9 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.kaizendeveloper.bitcoinsandbox.R
 import com.kaizendeveloper.bitcoinsandbox.blockchain.Block
-import com.kaizendeveloper.bitcoinsandbox.blockchain.BlockChain
 import com.kaizendeveloper.bitcoinsandbox.model.UserFactory
-import kotlinx.android.synthetic.main.activity_main.button
-import kotlinx.android.synthetic.main.activity_main.recycler
-import kotlinx.android.synthetic.main.activity_main.send
+import kotlinx.android.synthetic.main.activity_main.tabLayout
+import kotlinx.android.synthetic.main.activity_main.viewPager
 import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
@@ -23,28 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = UserFactory.activeUser?.name
 
-        BlockChain.addObserver { _, _ ->
-            recycler.adapter.notifyDataSetChanged()
-        }
-
-        recycler.layoutManager = LinearLayoutManager(baseContext).apply {
-            orientation = LinearLayoutManager.VERTICAL
-        }
-        recycler.adapter = BlockChainAdapter(BlockChain.blocks)
-
-        button.setOnClickListener {
-            showUsersDialog()
-        }
-
-        send.setOnClickListener {
-
-        }
-    }
-
-    private fun sendShowDialog() {
-
+        viewPager.adapter = BitCoinPagerAdapter(supportFragmentManager)
+        tabLayout.setupWithViewPager(viewPager)
     }
 
     private fun showUsersDialog() {
@@ -64,6 +45,31 @@ class MainActivity : AppCompatActivity() {
             title = UserFactory.activeUser?.name
         })
         builderSingle.show()
+    }
+
+    class BitCoinPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> BlockChainFragment()
+                1 -> TransactionsFragment()
+                2 -> TransferFragment()
+                3 -> UsersFragment()
+                else -> throw IllegalStateException("Illegal pager position")
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when (position) {
+                0 -> "Block chain"
+                1 -> "Transactions"
+                2 -> "Transfer"
+                3 -> "Users"
+                else -> throw IllegalStateException("Illegal pager position")
+            }
+        }
+
+        override fun getCount() = 4
     }
 
     class BlockChainAdapter(private val blocks: LinkedList<Block>) :
