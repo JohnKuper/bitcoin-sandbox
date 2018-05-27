45ee7aa4ab39a0e3298c095adf19bdb601661d73
Dmitry Korobeinikov <korobeinikov@yamoney.ru>
2018-05-27 22:56:56 +0300

    Added simple UI for block chain.

--- app/src/main/java/com/kaizendeveloper/bitcoinsandbox/blockchain/BlockChain.kt
+++ app/src/main/java/com/kaizendeveloper/bitcoinsandbox/blockchain/BlockChain.kt
@@ -1,12 +1,17 @@
 package com.kaizendeveloper.bitcoinsandbox.blockchain
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import com.kaizendeveloper.bitcoinsandbox.transaction.Mempool
 import com.kaizendeveloper.bitcoinsandbox.util.Cipher
 import java.util.LinkedList
 
 object BlockChain {
 
+    private val mutableBlocks = MutableLiveData<LinkedList<Block>>()
     private val blocks = LinkedList<Block>()
+
+    val observableBlocks: LiveData<LinkedList<Block>> = mutableBlocks
 
     //TODO add necessary checks for correct block
     fun processBlock(block: Block) {
@@ -24,5 +29,6 @@ object BlockChain {
 
     private fun addBlock(block: Block) {
         blocks.add(block)
+        mutableBlocks.postValue(blocks)
     }
 }
--- app/src/main/java/com/kaizendeveloper/bitcoinsandbox/ui/BlockChainFragment.kt
+++ app/src/main/java/com/kaizendeveloper/bitcoinsandbox/ui/BlockChainFragment.kt
@@ -1,16 +1,81 @@
 package com.kaizendeveloper.bitcoinsandbox.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.kaizendeveloper.bitcoinsandbox.R
+import com.kaizendeveloper.bitcoinsandbox.blockchain.Block
+import com.kaizendeveloper.bitcoinsandbox.util.toHexString
+import kotlinx.android.synthetic.main.fragment_blockchain.blockchain_list as blockChainList
 
 
 class BlockChainFragment : Fragment() {
 
+    private val blocksAdapter = BlocksAdapter(mutableListOf())
+    private lateinit var blockChainViewModel: BlockChainViewModel
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_blockchain, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecycler()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        blockChainViewModel = ViewModelProviders.of(this).get(BlockChainViewModel::class.java)
+        blockChainViewModel.blockChain.observe(this, Observer {
+            it?.also { blocksAdapter.setBlocks(it) }
+        })
+    }
+
+    private fun setupRecycler() {
+        blockChainList.apply {
+            adapter = blocksAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    //TODO Too many similar adapters. Probably should be improved.
+    inner class BlocksAdapter(private val blocks: MutableList<Block>) :
+        RecyclerView.Adapter<BlocksAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.item_block, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            with(blocks[position]) {
+                holder.hash.text = hash.toHexString()
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return blocks.size
+        }
+
+        fun setBlocks(blocks: List<Block>) {
+            with(this.blocks) {
+                clear()
+                addAll(blocks)
+                notifyDataSetChanged()
+            }
+        }
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val hash: TextView = view.findViewById(R.id.hash)
+        }
     }
 }
